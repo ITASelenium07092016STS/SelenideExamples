24df100fb81cfaa284a3ea2c244e6afb18f5fd59
Marta Verenchikova <martaverenchikova@yahoo.com>
2016-09-05 20:41:57 +0300

    New line was added

--- src/test/java/GoogleSearchTest.java
+++ src/test/java/GoogleSearchTest.java
@@ -11,6 +11,7 @@ public class GoogleSearchTest {
     @Test
     public void searchTest(){
         SearchPage searchPage = open("http://google.ru", SearchPage.class);
-        searchPage.searchFor("selenium").getElements().shouldHaveSize(11);
+        //searchPage.searchFor("selenium").getElements().shouldHaveSize(11);
+        searchPage.searchFor("selenium").getElements().shouldHaveSize(12);
     }
 }
